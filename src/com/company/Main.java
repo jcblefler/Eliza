a6979bb0f751cfca32f7f444dfacd253b47e8ebc
Jacob Lefler <jcb.lefler@gmail.com>
2019-04-25 09:38:54 -0400

    moved code to methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,52 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+    static String randomHedge(){
+        Random random = new Random();
+
+        ArrayList<String> hedge = new ArrayList<String>();
+
+        hedge.add("Why do you say that ");
+        hedge.add("You seem to think that ");
+        hedge.add("So, you are concerned that ");
+
+        int ranNum = random.nextInt(hedge.size());
+        return hedge.get(ranNum);
+
+    }
+
+    static ArrayList<String> wordReplacer(String input){
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.clear();
+
+        for (String inputs : input.split(" ")){
+            words.add(inputs);
+        }
+
+        for (int i = 0; i < words.size(); i++){
+            if (words.get(i).equalsIgnoreCase("i")){
+                words.set(i, "you");
+            } else if (words.get(i).equalsIgnoreCase("me")){
+                words.set(i, "you");
+            } else if (words.get(i).equalsIgnoreCase("my")){
+                words.set(i, "your");
+            } else if (words.get(i).equalsIgnoreCase("am")){
+                words.set(i, "are");
+            }
+        }
+        return words;
+
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<String> words = new ArrayList<String>();
 
         String responseText = "Enter your response here or Q to quit: ";
         String input = "";
@@ -18,29 +55,19 @@ public class Main {
         while(! input.equalsIgnoreCase("q")){
             System.out.print(responseText);
             input = scanner.nextLine();
-//            if (! input.equalsIgnoreCase("q")){
-//                break;
-//            }
 
-            for (String inputs : input.split(" ")){
-                words.add(inputs);
+            if (input.equalsIgnoreCase("q")){
+                break;
             }
-            for (int i = 0; i < words.size(); i++){
-                if (words.get(i).equalsIgnoreCase("i")){
-                    words.set(i, "you");
-                } else if (words.get(i).equalsIgnoreCase("me")){
-                    words.set(i, "you");
-                } else if (words.get(i).equalsIgnoreCase("my")){
-                    words.set(i, "your");
-                } else if (words.get(i).equalsIgnoreCase("am")){
-                    words.set(i, "are");
-                }
-                System.out.print(words.get(i) + " ");
+
+
+            System.out.print(randomHedge());
+            for (int i = 0; i < wordReplacer(input).size(); i++) {
+
+                System.out.print(wordReplacer(input).get(i) + " ");
             }
-            words.clear();
             System.out.println();
         }
-
 
     }
 }
